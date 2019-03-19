a3676f423b8b530b89992ca6bf2731c128fce1e8
Tomasz Bobula <tbobula@gmail.com>
2019-03-20 00:29:45 +0100

    clean up

--- country_iso.py
+++ country_iso.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 
@@ -11,24 +10,23 @@ def response_ok(res):
     )
 
 
-def get_page(url):
+def get_page(url: str):
     response = requests.get(url)
     if response_ok(response):
         return response.content
     else:
         return None
 
+
 url = 'https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'
 
 raw_html = get_page(url)
 html = BeautifulSoup(raw_html, 'html.parser')
 
-
-
-coutries = {}
-table = html.find('table',{'class':'wikitable sortable'}).find_all('td')
+countries = {}
+table = html.find('table', {'class': 'wikitable sortable'}).find_all('td')
 step = 0
-for index,elm in enumerate(table):
+for index, elm in enumerate(table):
     if 'id' in elm.attrs:
         code = elm.attrs['id']
     link = elm.find('a')
@@ -36,6 +34,8 @@ for index,elm in enumerate(table):
         continue
     if link.text[0] != '.' and link.text[:3] != 'ISO':
         country = link.text
-        coutries.update({code: country})
+        countries.update({code: country})
 
-print(coutries['FI'])
+print('Number of countries {}'.format(len(countries)))
+for key, value in countries.items():
+    print(key, value)
